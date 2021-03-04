f85a16eada49701c8c7938348257f4a7f2460451
Efra Espada <efraespada@gmail.com>
2021-03-04 22:01:32 +0100

    feature: working project

--- android/.cxx/cmake/debug/arm64-v8a/cmake_install.cmake
+++ android/.cxx/cmake/debug/arm64-v8a/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/efraespada/Documents/Github/stringcare/android
+# Install script for directory: /Users/efraespada/Documents/stringcare/android
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/efraespada/Documents/Github/stringcare/android/.cxx/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/efraespada/Documents/stringcare/android/.cxx/cmake/debug/arm64-v8a/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
