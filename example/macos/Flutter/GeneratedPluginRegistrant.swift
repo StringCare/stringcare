5631bbbaffdf2fc246309387b147ac13850d45f1
Efra Espada <efraespada@gmail.com>
2021-03-05 14:47:38 +0100

    macos support and readme

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import stringcare
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  StringcarePlugin.register(with: registry.registrar(forPlugin: "StringcarePlugin"))
+}
