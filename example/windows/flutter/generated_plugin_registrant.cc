0fa70a21f89e700482926e3274a231fed848404f
Efra Espada <efraespada@gmail.com>
2021-03-06 18:20:44 +0100

    windows support

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <stringcare/stringcare_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  StringcarePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("StringcarePlugin"));
+}
---
+++ windows/include/stringcare/stringcare_plugin.h
@@ -0,0 +1,23 @@
+#ifndef FLUTTER_PLUGIN_STRINGCARE_PLUGIN_H_
+#define FLUTTER_PLUGIN_STRINGCARE_PLUGIN_H_
+
+#include <flutter_plugin_registrar.h>
+
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
+#endif
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+FLUTTER_PLUGIN_EXPORT void StringcarePluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar);
+
+#if defined(__cplusplus)
+}  // extern "C"
+#endif
+
+#endif  // FLUTTER_PLUGIN_STRINGCARE_PLUGIN_H_
