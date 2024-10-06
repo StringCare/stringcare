1a4cd9ff4336b5f0b0d33f018ef035476efd932d
Efra Espada <efraespada@gmail.com>
2024-10-06 15:31:46 +0200

    feature: removed iml and 2 not valid tests

--- test/stringcare_test.dart
+++ test/stringcare_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:stringcare/src/native/stringcare_impl.dart' as native;
-import 'package:stringcare/src/web/stringcare_impl.dart' as web;
 import 'package:stringcare/stringcare.dart';
 
 void main() {
@@ -28,30 +26,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    var nativeImpl = native.StringcareImpl();
-    var webImpl = web.StringcareImpl();
-    print("nativeImpl sign: " + nativeImpl.testSign([]));
-    print("webImpl sign: " + webImpl.testSign([]));
     expect(await Stringcare().platformVersion, '42');
-  });
-
-  test('init hash match', () async {
-    var nativeImpl = native.StringcareImpl();
-    var webImpl = web.StringcareImpl();
-    var nHash = nativeImpl.testHash([]);
-    var wHash = webImpl.testHash([]);
-    print("native hash: $nHash");
-    print("web hash: $wHash");
-    expect(nHash, wHash);
-  });
-
-  test('sign match', () async {
-    var nativeImpl = native.StringcareImpl();
-    var webImpl = web.StringcareImpl();
-    var nSign = nativeImpl.testSign([]);
-    var wSign = webImpl.testSign([]);
-    print("native sign: $nSign");
-    print("web sign: $wSign");
-    expect(nSign, wSign);
   });
 }
